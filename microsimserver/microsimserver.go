c711a6e278c1c24ea173234a49f1b04fd4ea72c2
Ric Colasanti <ricardo.colasanti@glasgow.ac.uk>
2023-03-13 14:57:48 +0000

    compiled for mac

--- microsimserver/microsimserver.go
+++ microsimserver/microsimserver.go
@@ -28,64 +28,63 @@ func Output(msgType int, tag string) {
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("home")
-	http.ServeFile(w, r, "index.html")
-	// fmt.Fprintf(w, `
-	// <!doctype html>
-	// <html lang="en">
+	// http.ServeFile(w, r, "index.html")
+	fmt.Fprintf(w, `
+	<!doctype html>
+	<html lang="en">
 
-	// <head>
-	//   <title>MicosimViewer</title>
-	//   <!-- Required meta tags -->
-	//   <meta charset="utf-8">
-	//   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<head>
+	  <title>MicosimViewer</title>
+	  <!-- Required meta tags -->
+	  <meta charset="utf-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-	//   <!-- Bootstrap CSS -->
-	//   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-	// 	integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	// </head>
+	  <!-- Bootstrap CSS -->
+	  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+		integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	</head>
 
-	// <body>
-	//   <div class="container">
-	// 	<h3>MicrosimViewer static</h3>
-	// 	<input id="input" type="text" />
-	// 	<button onclick="send()">Send</button>
-	// 	<pre id="output"></pre>
+	<body>
+	  <div class="container">
+		<h3>MicrosimViewer static</h3>
+		<button onclick="send()">Clickme</button>
+		<pre id="output"></pre>
 
-	//   </div>
-	//   <!-- Optional JavaScript -->
-	//   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-	//   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-	// 	integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-	// 	crossorigin="anonymous"></script>
-	//   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-	// 	integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-	// 	crossorigin="anonymous"></script>
-	//   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-	// 	integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-	// 	crossorigin="anonymous"></script>
-	//   <script>
-	//   console.log("MicrosimServer");
-	//   var input = document.getElementById("input");
-	//   var output = document.getElementById("output");
-	//   var socket = new WebSocket("ws://127.0.0.1:5000/echo");
+	  </div>
+	  <!-- Optional JavaScript -->
+	  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+		integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+		crossorigin="anonymous"></script>
+	  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+		integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+		crossorigin="anonymous"></script>
+	  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+		integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+		crossorigin="anonymous"></script>
+	  <script>
+	  console.log("MicrosimServer");
+	  var input = document.getElementById("input");
+	  var output = document.getElementById("output");
+	  var socket = new WebSocket("ws://127.0.0.1:5555/echo");
 
-	//   socket.onopen = function () {
-	// 	  output.innerHTML += "Status: Connected\n";
-	//   };
+	  socket.onopen = function () {
+		  output.innerHTML += "Status: Connected\n";
+	  };
 
-	//   socket.onmessage = function (e) {
-	// 	  output.innerHTML += "Server: " + e.data + "\n";
-	//   };
+	  socket.onmessage = function (e) {
+		  output.innerHTML += "Server: " + e.data + "\n";
+	  };
 
-	//   function send() {
-	// 	  socket.send(input.value);
-	// 	  input.value = "";
-	//   }
-	//   </script>
-	// </body>
+	  function send() {
+		  socket.send("test");
+		  input.value = "";
+	  }
+	  </script>
+	</body>
 
-	// </html>
-	// `)
+	</html>
+	 `)
 }
 
 func PushData(w http.ResponseWriter, r *http.Request) {
